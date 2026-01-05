b15cb7b1684a8ce2ac6d8ee39bdcab5f047d6708
pitercoding <piterg.bio@gmail.com>
2026-01-05 13:26:01 +0100

    feat(projeto-final): adiciona interface Task e classes Todo e Reminder
    - cria interface Task com contrato comum de tarefas
    - implementa classes Todo e Reminder seguindo a interface
    - adiciona método render() polimórfico nas tarefas
    - ajusta TaskView para trabalhar com Task em vez de Object
    - ajusta TaskController para manipular lista tipada de Task

--- projeto-final/src/index.ts
+++ projeto-final/src/index.ts
@@ -1,16 +1,57 @@
 (() => {
-    const todo = {
-        description: 'todo',
-        done: false,
-    };
 
-    const reminder = {
-        description: 'reminder',
-        date: '05.01.2026',
-    };
+    interface Task {
+        id: string;
+        dateCreated: Date;
+        dateUpdated: Date;
+        description: string;
+        render(): string;
+    }
+
+    class Reminder implements Task {
+        id: string = '';
+        dateCreated: Date = new Date();
+        dateUpdated: Date = new Date();
+        description: string = '';
+
+        date: Date = new Date();
+        notifications: Array<string> = ['EMAIL'];
+
+        constructor(description: string, date: Date, notifications: Array<string>) {
+            this.description = description;
+            this.date = date;
+            this.notifications = notifications;
+        }
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+        
+    }
+
+    class Todo implements Task {
+        id: string = '';
+        dateCreated: Date = new Date();
+        dateUpdated: Date = new Date();
+        description: string = '';
+
+        done: boolean = false;
+
+        constructor(description: string) {
+            this.description = description;
+        }
+
+        render(): string {
+            return JSON.stringify(this);
+        }
+    }
+
+    const todo = new Todo('Todo criado com classe!')
+
+    const reminder = new Reminder('Reminder criado com classe!', new Date(), ['EMAIL']);
 
     const taskView = {
-        render(tasks: Array<Object>) {
+        render(tasks: Array<Task>) {
             const tasksList = document.getElementById('tasksList');
             while (tasksList?.firstChild) {
                 tasksList.removeChild(tasksList.firstChild);
@@ -18,7 +59,7 @@
 
             tasks.forEach((task) => {
                 const li = document.createElement('LI');
-                const textNode = document.createTextNode(JSON.stringify(task));
+                const textNode = document.createTextNode(task.render());
                 li.appendChild(textNode);
                 tasksList?.appendChild(li);
             });
@@ -26,7 +67,7 @@
     };
 
     const TaskController = (view: typeof taskView) => {
-        const tasks: Array<Object> = [todo, reminder];
+        const tasks: Array<Task> = [todo, reminder];
 
         const handleEvent = (event: Event) => {
             event.preventDefault();
